62d162f1e399ebcee7a5a06271573f1993c7ac0f
robertreed4501 <robertreed4501@gmail.com>
2022-09-06 14:47:10 -0400

    updated post endpoint to return ReceiptResponse object

--- src/main/java/com/robertreed4501/chores/controller/ReceiptController.java
+++ src/main/java/com/robertreed4501/chores/controller/ReceiptController.java
@@ -1,6 +1,8 @@
 package com.robertreed4501.chores.controller;
 
 import com.robertreed4501.chores.model.db.Receipt;
+import com.robertreed4501.chores.model.http.requests.ReceiptRequest;
+import com.robertreed4501.chores.model.http.response.ReceiptResponse;
 import com.robertreed4501.chores.service.ReceiptService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +17,9 @@ public class ReceiptController {
     private final ReceiptService receiptService;
 
     @PostMapping
-    public String addReceipt(@RequestBody Long assignmentId){
-        receiptService.addReceipt(assignmentId);
-        return "receipt added.";
+    public ReceiptResponse addReceipt(@RequestBody ReceiptRequest request){
+        receiptService.addReceipt(request.getAssignmentId());
+        return new ReceiptResponse("receipt added.",null);
     }
 
     @GetMapping
