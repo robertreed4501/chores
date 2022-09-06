863e25497595ea7edfad48e62ccf02bcd6bbc8cd
robertreed4501 <robertreed4501@gmail.com>
2022-09-06 14:48:26 -0400

    added endpoint to return user by key

--- src/main/java/com/robertreed4501/chores/controller/UserController.java
+++ src/main/java/com/robertreed4501/chores/controller/UserController.java
@@ -2,23 +2,30 @@ package com.robertreed4501.chores.controller;
 
 import com.robertreed4501.chores.model.db.User;
 import com.robertreed4501.chores.model.db.UserGroup;
+import com.robertreed4501.chores.model.http.response.LoginResponse;
+import com.robertreed4501.chores.model.http.response.UsersInGroupResponse;
 import com.robertreed4501.chores.repository.UserGroupRepository;
 import com.robertreed4501.chores.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
     private final UserService userService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/mygroup")
-    public List<User> getUsersByGroupId(Long id){
+    public List<UsersInGroupResponse> getUsersByGroupId(@RequestParam Long id){
         return userService.getUsersByGroupId(id);
+    }
+
+    @GetMapping
+    public LoginResponse getUserByKey(@RequestHeader("key") String key) {
+        return userService.getUserLoginResponseByApiKey(key);
     }
 }
