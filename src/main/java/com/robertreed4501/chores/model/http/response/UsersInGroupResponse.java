111fb3c74b2832139f5a1870e592afdcae4334ae
robertreed4501 <robertreed4501@gmail.com>
2022-09-06 14:47:41 -0400

    created more response objects

---
+++ src/main/java/com/robertreed4501/chores/model/http/response/UsersInGroupResponse.java
@@ -0,0 +1,14 @@
+package com.robertreed4501.chores.model.http.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsersInGroupResponse {
+
+    private Long id;
+    private String firstName;
+}
