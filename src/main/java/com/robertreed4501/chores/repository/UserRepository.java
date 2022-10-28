9e47b9ee6af48094f238cc7472b98cb94ac99980
robertreed4501 <robertreed4501@gmail.com>
2022-10-28 11:54:04 -0400

    added method to check if an email is already registered

--- src/main/java/com/robertreed4501/chores/repository/UserRepository.java
+++ src/main/java/com/robertreed4501/chores/repository/UserRepository.java
@@ -18,6 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     //@Query(value = "SELECT * FROM user WHERE api_key = :apiKey", nativeQuery = true)
     User findUserByApiKey(String apiKey);
 
-
+    boolean existsByEmail(String email);
 
 }
