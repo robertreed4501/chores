341fa99eabef476fb1d6869b4b6127607be65b19
robertreed4501 <robertreed4501@gmail.com>
2022-11-07 21:36:40 -0500

    added methods to MessageService

--- src/main/java/com/robertreed4501/chores/repository/MessageRepository.java
+++ src/main/java/com/robertreed4501/chores/repository/MessageRepository.java
@@ -2,10 +2,21 @@ package com.robertreed4501.chores.repository;
 
 
 import com.robertreed4501.chores.model.db.Message;
+import com.robertreed4501.chores.model.db.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-	
+
+    List<Message> findAllByUserIdTo (Long id);
+
+    List<Message> findAllByUserIdFrom (Long id);
+
+    List<Message> findAllByUserIdToAndDeleted (User user, Boolean deleted);
+
+    List<Message> findAllByUserIdFromAndDeleted (User user, Boolean deleted);
+
 }
