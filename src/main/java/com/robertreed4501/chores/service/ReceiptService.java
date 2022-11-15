3dba346e4d4f347a0e3549090e806809cf94805c
robertreed4501 <robertreed4501@gmail.com>
2022-11-14 19:03:18 -0500

    added assignment.setDone(true) while creating receipt.  in the process of changing data model to not need receipts.

--- src/main/java/com/robertreed4501/chores/service/ReceiptService.java
+++ src/main/java/com/robertreed4501/chores/service/ReceiptService.java
@@ -1,24 +1,31 @@
 package com.robertreed4501.chores.service;
 
+import com.robertreed4501.chores.model.db.Assignment;
 import com.robertreed4501.chores.model.db.Receipt;
 import com.robertreed4501.chores.repository.ReceiptRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class ReceiptService {
 
     private final AssignmentService assignmentService;
     private final ReceiptRepository receiptRepository;
 
+    @Modifying
     public String addReceipt(Long assignmentId){
+        Assignment assignment = assignmentService.getAssignmentById(assignmentId);
+        assignment.setDone(true);
         receiptRepository.save(
                 new Receipt(
-                        assignmentService.getAssignmentById(assignmentId),
+                        assignment,
                         LocalDateTime.now(),
                         false,
                         false
@@ -30,4 +37,5 @@ public class ReceiptService {
     public List<Receipt> getAllReceipts() {
         return receiptRepository.findAll();
     }
+
 }
