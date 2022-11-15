f188feca79187ed7844c9a44647c782b0be3e71b
robertreed4501 <robertreed4501@gmail.com>
2022-11-14 19:00:55 -0500

    added spring security for bcrypt only, so needed to exclude SecurityAutoConfiguration

--- src/main/java/com/robertreed4501/chores/ChoresApplication.java
+++ src/main/java/com/robertreed4501/chores/ChoresApplication.java
@@ -2,11 +2,12 @@ package com.robertreed4501.chores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ChoresApplication {
 
 	public static void main(String[] args) {
