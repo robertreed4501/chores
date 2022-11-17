d76afb87537bf88402f63e0fed6bea65d2681ef1
robertreed4501 <robertreed4501@gmail.com>
2022-11-16 23:11:05 -0500

    created db call to find assignments by user and chore

--- src/main/java/com/robertreed4501/chores/repository/AssignmentRepository.java
+++ src/main/java/com/robertreed4501/chores/repository/AssignmentRepository.java
@@ -3,23 +3,19 @@ package com.robertreed4501.chores.repository;
 import com.robertreed4501.chores.model.db.Assignment;
 import com.robertreed4501.chores.model.db.Chore;
 import com.robertreed4501.chores.model.db.User;
-import org.hibernate.annotations.SQLUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
-    public List<Assignment> getAssignmentsByUser(User user);
+    List<Assignment> getAssignmentsByUser(User user);
 
     @Override
-    void deleteById(Long aLong);
+    void deleteById(Long id);
 
     @Modifying
     @Query(value = "UPDATE Assignment SET active=0 WHERE id=?1")
@@ -29,8 +25,5 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     @Query(value = "UPDATE Assignment SET active=0 WHERE user.id=?1")
     int setAllInactive(Long id);
 
-    //@Query(value = "SELECT Assignment FROM Assignment WHERE user.id=?1 AND chore.id=?2")
-    Optional<List<Assignment>> findByUserAndChoreAndActive(User user, Chore chore, boolean active);
-
-
+    List<Assignment> findAssignmentsByChore (Chore chore);
 }
