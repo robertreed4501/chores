cad98146c6cf447c0ff90d6e9ee36450309555f5
robertreed4501 <robertreed4501@gmail.com>
2022-11-16 23:19:21 -0500

    changed password validation, changed method name from Login to login

--- src/main/java/com/robertreed4501/chores/service/LoginService.java
+++ src/main/java/com/robertreed4501/chores/service/LoginService.java
@@ -3,9 +3,9 @@ package com.robertreed4501.chores.service;
 import com.robertreed4501.chores.model.db.User;
 import com.robertreed4501.chores.model.http.requests.LoginRequest;
 import com.robertreed4501.chores.model.http.response.LoginResponse;
+import com.robertreed4501.chores.utilities.PasswordValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +19,9 @@ public class LoginService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final PasswordValidator passwordValidator;
 
-    public LoginResponse Login(LoginRequest request) {
+    public LoginResponse login(LoginRequest request) {
         String username = request.getUsername();
         String password = request.getPassword();
         LoginResponse response;
@@ -33,7 +34,8 @@ public class LoginService {
         System.out.println(password + " - password, " + user.getPassword() + " - user.getPassword() - LoginService.java");
         System.out.println(passwordEncoder.matches(request.getPassword(), user.getPassword()) + " - bool for checking pw against hash");
 /*        if (!password.equals(user.getPassword())) {*/
-            if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+            //if (!BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+                if (!passwordValidator.checkPassword(request.getPassword(), user.getPassword())) {
             response = new LoginResponse(null, "invalid password");
         } else{
             String key = UUID.randomUUID().toString();
