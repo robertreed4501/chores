83a29599fcde6e5e08279f170e7798e56094c5a2
robertreed4501 <robertreed4501@gmail.com>
2022-11-16 23:14:29 -0500

    moved cors config to WebConfig.java

--- src/main/java/com/robertreed4501/chores/ChoresApplication.java
+++ src/main/java/com/robertreed4501/chores/ChoresApplication.java
@@ -14,14 +14,5 @@ public class ChoresApplication {
 		SpringApplication.run(ChoresApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
-			}
 
-		};
-	}
 }
