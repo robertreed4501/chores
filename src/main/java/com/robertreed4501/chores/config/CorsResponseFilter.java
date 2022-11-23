a37faf454b3e71beacb26cc57d48faef10c195a9
robertreed4501 <robertreed4501@gmail.com>
2022-11-23 13:11:18 -0500

    created filter to add {'Access-Control-Allow-Origin': '*'} to headers

---
+++ src/main/java/com/robertreed4501/chores/config/CorsResponseFilter.java
@@ -0,0 +1,23 @@
+package com.robertreed4501.chores.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsResponseFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
