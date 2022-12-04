c6421ee18dc457e7a9ad0a7c1868230530c32e5b
robertreed4501 <robertreed4501@gmail.com>
2022-12-04 11:18:38 -0500

    fiddling with cors

--- src/main/java/com/robertreed4501/chores/ChoresApplication.java
+++ src/main/java/com/robertreed4501/chores/ChoresApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @CrossOrigin
@@ -21,5 +22,14 @@ public class ChoresApplication {
 		/*SpringApplication.run(ChoresApplication.class, args);*/
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://choresgalore.site/");
+			}
+		};
+	}
 
 }
