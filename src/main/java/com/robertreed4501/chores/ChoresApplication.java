55139dc0221bc67482f3872f7b11b7792f20a9fc
robertreed4501 <robertreed4501@gmail.com>
2022-12-04 11:11:31 -0500

    fiddling with cors

--- src/main/java/com/robertreed4501/chores/ChoresApplication.java
+++ src/main/java/com/robertreed4501/chores/ChoresApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@CrossOrigin
 public class ChoresApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/robertreed4501/chores/config/WebConfig.java
+++ src/main/java/com/robertreed4501/chores/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig {
@@ -16,12 +17,11 @@ public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://choresgalore.site").allowedMethods("*");
+                registry.addMapping("/**").allowedOrigins("https://choresgalore.site");
             }
-
         };
     }
 }
