7cc3c3da60055144993b7b77e49a847be20becfb
robertreed4501 <robertreed4501@gmail.com>
2022-12-06 12:56:22 -0500

    fiddling with cors

--- src/main/java/com/robertreed4501/chores/config/MyCorsConfig.java
+++ src/main/java/com/robertreed4501/chores/config/MyCorsConfig.java
@@ -20,7 +20,7 @@ public class MyCorsConfig implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, key, access-control-allow-origin," +
-                "access-control-allow-methods, access-control-allow-headers");
+                "access-control-allow-methods, access-control-allow-headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers");
         response.setHeader("Access-Control-Max-Age", "3600");
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
